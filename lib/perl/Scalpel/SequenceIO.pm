400309246121a44c8fff89d24e6d060485dcf7f4
Luke Goodsell <l.goodsell@achillestx.com>
2017-05-14 14:39:04 +0100

    Removed dependencies, restructured repository

---
+++ lib/perl/Scalpel/SequenceIO.pm
@@ -0,0 +1,552 @@
+package Scalpel::SequenceIO;
+
+###################################################################
+# SequenceIO
+#
+# Package for common IO routines
+# 
+#  Author: Giuseppe Narzisi 
+#    Date: December 11, 2013
+#
+###################################################################
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(loadCoordinates loadRegions loadVCF loadExonsBed loadGenomeFasta parseHeader saveReadGroup extractCoords extractCoordsDB extractSM);
+
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use lib $Bin; # add $Bin directory to @INC
+use Utils;
+use HashesIO;
+use Digest::MD5 qw(md5_hex);
+
+my $bamtools = "$Bin/bamtools-2.3.0/bin/bamtools";
+
+## extract coordinates from file
+#####################################################
+sub extractCoords {
+
+	my $in = $_[0];
+	my $out = $_[1];
+	
+	print STDERR "Extract coordinates of candidates...";
+	
+	open IN, "< $in" or die "Can't open $in ($!)\n";
+	open OUT, "> $out" or die "Can't open $out ($!)\n";
+	
+	my $cnt = 0;
+	while (<IN>) {
+		chomp;
+		next if ($_ =~ /^#/); # skip comments
+		
+		my @A = split /\t/, $_;
+		print OUT "$A[0]\t$A[1]\n";
+		$cnt++
+	}
+	
+	close IN;
+	close OUT;
+	
+	return $cnt; 
+}
+
+## extract coordinates from DB
+#####################################################
+sub extractCoordsDB {
+	
+	my $dbfile = $_[0];
+	my $bedfile = $_[1];
+	my $intarget = $_[2];
+	my $out = $_[3];
+	
+	my %exons;
+	my %variants;
+	my %dups; # used to remove duplicated locations
+	
+	print STDERR "Extract coordinates of candidates...";
+	
+	loadRegions("$bedfile", \%exons, 0, 0);
+	loadDB("$dbfile", \%variants, \%exons, $intarget);
+	
+	open OUT, "> $out" or die "Can't open $out ($!)\n";
+
+	my $cnt = 0;
+	foreach my $key (keys %variants) {
+		my $mut = $variants{$key};
+
+		next if($mut->{type} eq "snp"); # skip SNPs
+				
+		my $chr = $mut->{chr};
+		my $pos = $mut->{pos};
+		my $loc = "$chr\t$pos";
+		
+		if( !(exists $dups{$loc}) ) {
+			print OUT "$loc\n";
+			$cnt++;
+			$dups{$loc} = 1;
+		}
+	}
+	
+	close OUT;
+	
+	return $cnt; 
+}
+
+
+## load selected location (coordinates)
+#####################################################
+sub loadCoordinates {
+	
+	my $file = $_[0];
+	my $exons = $_[1];
+	my $VERBOSE = $_[2];
+	
+	print STDERR "Loading coordinates...";
+	
+	my $cntall_locs = 0;
+	if ($file ne "null") {
+		 
+		open SELECTED, "< $file" or die "Can't open $file ($!)\n";
+
+		while (<SELECTED>) {
+			chomp;
+			next if ($_ =~ /^#/); # skip comments
+			
+			my ($chr, $pos) = split /\t/, $_, 2;
+			#if ($chr =~ /^chr/) { $chr = substr($chr,3); }
+			
+			my $exon;
+			$exon->{chr} = $chr;
+			$exon->{start} = $pos;
+			$exon->{end} = $pos;
+			
+			push @{$exons->{$chr}}, $exon;
+			$cntall_locs++;
+		}
+		close SELECTED;
+
+		#if($VERBOSE) {
+			print STDERR "$cntall_locs locations.\n";
+		#}
+	}
+	return $cntall_locs;
+}
+
+## load regions to process
+#####################################################
+sub loadRegions {
+	
+	my $bedfile = $_[0];
+	my $exons = $_[1];
+	my $radius = $_[2];
+	my $VERBOSE = $_[3];
+	
+	my $cnt = 0;
+	
+	if (-e "$bedfile") {
+		$cnt = loadExonsBed("$bedfile", $exons, $radius, $VERBOSE);
+	}
+	elsif ($bedfile =~ m/(\w+):(\d+)-(\d+)/) { # try to parse as region (e.g., 1:1234-4567)
+	
+		print STDERR "Loading targets region: $bedfile\n";
+	
+		#parse region
+		my ($chr,$interval) = split(':',$bedfile);
+		my ($start,$end) = split('-',$interval);
+
+		my $exon;
+		$exon->{chr} = $chr;
+		$exon->{start} = $start;
+		$exon->{end} = $end;
+
+		push @{$exons->{$chr}}, $exon;
+		
+		$cnt = 1;
+	}
+	else {
+		print STDERR "Error: unrecognized string in --bed option: $bedfile\n";
+		exit;
+	}
+	
+	return $cnt;
+}
+
+## load exons list
+#####################################################
+sub loadExonsBed {
+	
+	print STDERR "Loading targets from BED file...";
+
+	my $file = $_[0];
+	my $exons = $_[1];
+	my $radius = $_[2];
+	my $VERBOSE = $_[3];
+	
+	open EXONSLIST, "< $file" or die "Can't open $file ($!)\n";
+	
+	my $cntall_exons = 0;
+	my $cntovl_exons = 0;
+
+	my $prev_exon;
+	$prev_exon->{chr} = 0;
+	$prev_exon->{start} = 0;
+	$prev_exon->{end} = 0;
+	while (<EXONSLIST>)
+	{
+		chomp;
+		next if ($_ =~ /^#/ || $_ =~ /^@/); # skip comments
+		
+		#my ($chr, $start, $end) = split /\t/, $_, 3;
+		my @array = split /\t/, $_;
+		my $chr = $array[0];
+		my $start = $array[1];
+		my $end = $array[2];
+		#if ($chr =~ /^chr/) { $chr = substr($chr,3); }
+
+		# exons coordinate are sorted in the bed file
+		if ( ($prev_exon->{chr} eq $chr) && ($start <= $prev_exon->{end}) ) { 
+			#print STDERR "exons regions overlap: [$prev_exon->{start},$prev_exon->{end}] [$start,$end]\n";
+			$prev_exon->{end} = $end;
+			$cntovl_exons++;
+		}
+		else {
+			my $exon;
+			$exon->{chr} = $chr;
+			$exon->{start} = $start;
+			$exon->{end} = $end;
+			
+			## extend region left and right by radius
+			#my $l = $start-$radius;
+			#my $u = $end+$radius;
+	
+			push @{$exons->{$exon->{chr}}}, $exon;
+			
+			# update prev exon
+			$prev_exon = $exon;
+			
+			$cntall_exons++;
+		}
+		#last if($cntall_exons > 10);
+	}
+	
+	if($VERBOSE) {
+		# print exons
+		foreach my $k (keys %$exons) { # for each chromosome
+			foreach my $exon (@{$exons->{$k}}) { # for each exon
+				print STDERR "$exon->{chr}\t$exon->{start}\t$exon->{end}\n";
+			}
+		}
+	}
+
+	close EXONSLIST;
+
+	#if($VERBOSE) {
+		print STDERR "$cntall_exons targets (filtered $cntovl_exons overlapping).\n";
+	#}
+	
+	return $cntall_exons;
+}
+
+# load VCF file
+##########################################
+sub loadVCF {
+    
+    print STDERR "Loading mutations from VCF file...";
+    
+    my $file = $_[0];
+	my $hash = $_[1];
+	my $REF  = $_[2];
+	my $FAIDX = $_[3];
+	my $genome = $_[4];
+	
+    open VCF, "< $file" or die "Can't open $file ($!)\n";
+
+	my $cnt = 0;
+    my $header = "";
+    while ( <VCF> ) {
+        chomp;
+        #print "$_\n";
+        if($_ =~ m/^#/) { 
+			$header = $_;
+            #print "$header\n";
+        }
+        else { 
+			#2       154110840       .       ATTTTTT ATTTTT  3.45209 .       INDEL;IDV=1;IMF=0.0909091;DP=11;VDB=0.135767;SGB=-0.379885;MQSB=0.763675;MQ0F=0;AC=0;AN=2;DP4=6,4,1,0;MQ=56     GT:PL   0/0:0,17,82
+            my @fields = split("\t", $_);
+			my $chr = $fields[0];
+			my $pos = $fields[1];
+			my $ref = $fields[3];
+			my $alts = $fields[4];
+			my $info = $fields[7];
+			
+			# extract supporting coverage fror the allele
+			# IDV (=Maximum number of reads supporting an indel) and IMF (=Maximum fraction of reads supporting an indel)
+			my @I = split(';', $info);
+			
+			my $idv = 0;
+			my $imf = 0;
+			#my $dp  = 0;
+			foreach (@I) {
+				my ($tag,$value) = split ('=',$_);		
+				if($tag eq "IDV") { $idv = $value; }
+				if($tag eq "IMF") { $imf = $value; }
+				#if($tag eq "DP")  { $dp = $value; }			
+			} 
+			
+			# process each alternative allele
+			my @A = split(',', $alts);
+							
+			foreach (@A) { 
+				#$cnt++;
+				
+				my $alt = $_;		
+					
+				my $type;
+				my $newref;
+				my $newalt;
+				my $prevbpalt;
+				my $prevbpref;
+				my $len;
+				if(length($ref)==1 && length($alt)==1 ) {
+					$type = "snp";
+					$len = 1;
+					$newref = $ref;
+				 	$newalt = $alt;
+				}
+				elsif(length($ref) > length($alt)) {
+					$type = "del";
+					#my $idx = index($ref, $alt);
+					#$newref = substr($ref,length($alt));
+					#$len = length($newref);
+					$len = length($ref)-length($alt);
+					$newalt = ('-' x $len);
+					$newref = substr($ref,1,$len);
+					$prevbpref = substr($ref,0,1);
+					$prevbpalt = $prevbpref;
+					$pos+=1;
+				}
+				elsif(length($ref) < length($alt)) {
+					$type = "ins";
+					#my $idx = index($alt, $ref, 1);
+					#$newalt = substr($alt,length($ref));
+					#$len = length($newalt);
+					$len = length($alt)-length($ref);
+					$newalt = substr($alt,1,$len);
+					$prevbpalt = substr($alt,0,1);
+					$prevbpref = $prevbpalt;
+					$newref = ('-' x $len);
+					$pos+=1;
+				}
+			
+				my $mut;
+				$mut->{chr}  = $chr;
+				$mut->{pos}  = $pos;
+				$mut->{ref}  = $newref;
+				$mut->{seq}  = $newalt;
+				$mut->{type} = $type;
+				$mut->{len}  = $len;
+				$mut->{idv}  = $idv;
+				$mut->{imf}  = $imf;
+				$mut->{prevbpref} = $prevbpref;
+				$mut->{prevbpalt} = $prevbpalt;
+				$mut->{avgcov} = $idv;
+				$mut->{mincov} = $idv;
+				$mut->{status} = "ok";
+				$mut->{zygosity} = "na";
+				$mut->{altcov} = 0;
+				$mut->{inheritance} = "na";
+
+				leftNormalize(\$mut, 300, $REF, $FAIDX, $genome);
+				#print STDERR "$mut->{chr}\t$mut->{pos}\t$mut->{ref}\t$mut->{seq}\t$mut->{type}\t$mut->{len}\n";
+			
+				#my $ref_encoded = md5_hex($mut->{ref});
+				#my $qry_encoded = md5_hex($mut->{seq});
+			
+				#my $key = sprintf("%s:%d:%s:%d:%s:%s", $mut->{chr}, $mut->{pos}, $mut->{type}, $mut->{len}, $ref_encoded, $qry_encoded);
+				my $key_long = sprintf("%s:%d:%s:%d:%s:%s", $mut->{chr}, $mut->{pos}, $mut->{type}, $mut->{len}, $mut->{ref}, $mut->{seq});
+				my $key = md5_hex($key_long);
+			
+				if( !(exists $hash->{$key}) ) {
+					$hash->{$key} = $mut;
+					$cnt++;
+				}
+			}
+        }
+    }
+    close VCF;
+
+	print STDERR "$cnt mutations.\n";
+}
+
+# load the genome in fasta format
+##########################################
+sub loadGenomeFasta {
+    
+    print STDERR "Loading genome from FASTA file...";
+    
+    my $file = $_[0];
+    my $genome = $_[1];
+	    
+    open FASTAFILE, "< $file" or die "Can't open $file ($!)\n";
+
+    my $header = "";
+    my $SDNA = "";
+    my $chr = "";
+	my $cnt = 0;
+    while ( <FASTAFILE> ) {
+        chomp;
+        #print "$_\n";
+        if($_ =~ m/>/) {
+			$cnt++;
+            $header = $_;
+            #print "$header\n";
+            if($chr ne "") {
+                #print "$chr\n";
+                $genome->{$chr}->{seq} = $SDNA;
+            }
+            
+            $header =~ s/^>//; # remove ">"
+            $header =~ s/\s+$//; # remove trailing whitespace
+
+            my ($label, $tmp) = split / /, $header, 2;
+			$chr = $label;			
+			#if ($label =~ /^chr/) { $chr = substr($label,3); } # update chromosome label
+            $SDNA = "";# clear out old sequence
+        }
+        else { 
+            s/\s+//g; # remove whitespace
+            $SDNA .= $_; 
+        }
+    }
+    close FASTAFILE;
+
+    if($chr ne "") { # handle last sequence
+        #print "$chr\n";
+        $genome->{$chr}->{seq} = $SDNA;
+    }  
+
+	#if($VERBOSE) {
+		print STDERR "$cnt sequences.\n";
+	#}
+}
+
+
+## extract SM info from BAM header
+#####################################################
+sub extractSM {
+	 
+	my $PREFIX = $_[0];
+	my $SAMPLE = $_[1];
+	my $headerfile = "header.txt";
+	#my $headerfile = "header.".$SAMPLE.".txt";
+	
+	my $SM = "";
+	
+	## extract header from SAM file
+	#runCmd("extract SAM header", "$bamtools header -in $PREFIX/bamfile.bam > $PREFIX/$headerfile");
+	
+	## read the list of read groups per sample from header
+	open HEADER, "< $PREFIX/$SAMPLE/$headerfile" or die "Can't open $PREFIX/$SAMPLE/$headerfile ($!)\n";
+	
+	while (<HEADER>) {
+		chomp;
+		next if($_ eq ""); # skip empty string
+		next if($_ !~ /^\@/); # skip non-header line
+		
+		my @records = split /\t/, $_;
+	  	if ($records[0] eq "\@RG") {
+			#@RG     ID:READGROUP    SM:SAMPLE
+			
+	  		#print join(' ', @records), "\n";
+	  		
+	  		my $sm;
+	  		my $id;
+	  		foreach my $tag (@records) {
+	  			my @data = split /:/, $tag;
+	  			#print join(':', @data), "\n";
+	  			if($data[0] eq "SM") { $SM = $data[1]; }
+	  			#if($data[0] eq "ID") { $id = $data[1]; }
+	  		}
+	  	}
+	}
+	close HEADER;
+	
+	return $SM;
+}
+
+## process SAM header to extract read groups info
+#####################################################
+sub parseHeader {
+	 
+	my $PREFIX = $_[0];
+	my $readgroups = $_[1];
+	my $headerfile = "header.txt";
+	
+	# erease readgroup information from previous family
+	for (keys %$readgroups) { delete $readgroups->{$_}; }
+	
+	## extract header from SAM file
+	runCmd("extract SAM header", "$bamtools header -in $PREFIX/bamfile.bam > $PREFIX/$headerfile");
+	
+	## read the list of read groups per sample from header
+	open HEADER, "< $PREFIX/$headerfile" or die "Can't open $PREFIX/$headerfile ($!)\n";
+	
+	while (<HEADER>) {
+		chomp;
+		next if($_ eq ""); # skip empty string
+		next if($_ !~ /^\@/); # skip non-header line
+		
+		my @records = split /\t/, $_;
+	  	if ($records[0] eq "\@RG") {
+			#@RG     ID:READGROUP    SM:SAMPLE
+			
+	  		#print join(' ', @records), "\n";
+	  		
+	  		my $sm;
+	  		my $id;
+	  		foreach my $tag (@records) {
+	  			my @data = split /:/, $tag;
+	  			#print join(':', @data), "\n";
+	  			if($data[0] eq "SM") { $sm = $data[1]; }
+	  			if($data[0] eq "ID") { $id = $data[1]; }
+	  		}
+	  		push @{$readgroups->{$sm}->{rg}}, $id;
+	  	}
+	}
+	close HEADER;
+}
+
+## save read groups to file
+#####################################################
+sub saveReadGroup {
+	
+	my $sample = $_[0];
+	my $PREFIX = $_[1];
+	my $readgroups = $_[2];
+	my $rgfile = $_[3];
+	
+	open SAMPLE, "> $PREFIX/$rgfile" or die "Can't open $PREFIX/$rgfile ($!)\n";
+	
+	for my $sm ( keys %$readgroups ) {
+		
+		if($sample eq "ALL") {
+			#print $sample.": ";
+			foreach my $rg ( @{$readgroups->{$sm}->{rg}}) {
+				#	print $rg.", ";
+				print SAMPLE "$rg\n"; 
+			}
+		}
+		elsif($sm eq $sample) {
+			#print $sample.": ";
+			foreach my $rg ( @{$readgroups->{$sm}->{rg}}) {
+				#	print $rg.", ";
+				print SAMPLE "$rg\n"; 
+			}
+		}
+	}
+	close SAMPLE;
+}
+
+1;
---
+++ lib/perl/Scalpel/Utils.pm
@@ -0,0 +1,330 @@
+package Scalpel::Utils;
+
+###################################################################
+# Utils
+#
+# Package with basic commons routines
+# 
+#  Author: Giuseppe Narzisi 
+#    Date: December 11, 2013
+#
+###################################################################
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw(runCmd leftNormalize uniq binarySearch inTarget bychrpos elapsedTime fisher_yates_shuffle Log10 genotype);
+@EXPORT_OK = qw($findVariants $findDenovos $findSomatic $exportTool $bamtools $samtools $bcftools);
+
+use strict;
+use warnings;
+use POSIX;
+use FindBin qw($Bin);
+use lib $Bin; # add $Bin directory to @INC
+
+# programs via absolute path
+our $findVariants = "$Bin/FindVariants.pl";
+our $findDenovos  = "$Bin/FindDenovos.pl";
+our $findSomatic  = "$Bin/FindSomatic.pl";
+#our $exportTool   = "$Bin/ExportVariants.pl";
+our $exportTool   = "$Bin/scalpel-export";
+our $bamtools     = "$Bin/bamtools-2.3.0/bin/bamtools";
+our $samtools     = "$Bin/samtools-1.1/samtools";
+our $bcftools     = "$Bin/bcftools-1.1/bcftools";
+
+# Run system command 
+#####################################################
+
+sub runCmd 
+{ 
+	my $info = $_[0];
+	my $cmd  = $_[1];
+	#my $VERBOSE = $_[2];
+	
+	my $retval = 0;
+
+	#print STDERR "$info ($cmd)...\n" if $VERBOSE;
+
+	my $rc = system($cmd); 
+	#die "failed ($rc)\n" if $rc; 
+	if ($rc) { 
+		print STDERR "Command failure: $info ($cmd)...\n";
+		
+		$retval = -1;
+		
+		if ($? == -1) {
+			print "failed to execute: $!\n";
+		}
+		elsif ($? & 127) {
+			printf "child died with signal %d, %s coredump\n",
+			($? & 127),  ($? & 128) ? 'with' : 'without';
+		}
+		else {
+			printf "child exited with value %d\n", $? >> 8;
+		}	
+	}
+	return $retval;
+}
+
+## Normalize indel location to leftmost position
+#####################################################
+
+sub leftNormalize {
+	my $indel = ${$_[0]};
+	my $delta = $_[1];
+	my $REF = $_[2];
+	my $FAIDX = $_[3];
+	my $genome = $_[4];
+	
+	my $pos = $indel->{pos};
+	my $type = $indel->{type};
+	my $chr = $indel->{chr};
+	my $indel_len = $indel->{len};
+
+	## extract sequence around indel from reference file
+	#pos = $pos+1; # convert from 0-based to 1-based coordinate system
+	my $l = $pos-$indel_len-$delta;
+ 	my $r = $pos+$indel_len+$delta;
+		
+	my $reference = "";
+	if ($FAIDX == 0) {
+		$reference = substr($genome->{$chr}->{seq}, $l-1, $r-$l+1);
+	}
+	else {
+		$reference = readpipe( "$samtools faidx $REF $chr:$l-$r | awk '\$0 !~ /^>/'" ); # 0-based coordinate system
+	}
+	$reference=~s/\R//g; #remove newlines
+	#print "samtools faidx $REF $chr:$L-$R\n";
+	#print STDERR "$reference\n";
+ 	my $p = $indel_len+$delta;
+	my $new_pos = $pos;
+	if($type eq "del") { # deletion
+		my $left_seq  = substr($reference, 0, $p);
+		my $right_seq = substr($reference, $p+$indel_len);
+		my $true_indel_seq = $left_seq . $right_seq;
+		$new_pos = $l+$p;
+		$indel->{ref} = substr($reference, $p, $indel_len);
+		
+		## reposition the candidate variant to a canonical (leftmost) position
+		my $left_hyplotype;
+		my $right_hyplotype;
+		my $new_indel_seq;
+		for (my $sft = 1; $sft <= $delta; $sft++) {
+			my $i = $p-$sft;
+			$left_hyplotype  = substr($reference, 0, $i);
+		  	$right_hyplotype = substr($reference, $i+$indel_len);
+		  	$new_indel_seq = $left_hyplotype . $right_hyplotype;
+		  	#print "$true_indel_seq";
+		    #print "$new_indel_seq";
+		   	if ($true_indel_seq eq $new_indel_seq) {
+				#print "Repositioning!\n";
+		  		$new_pos = $l+$i; # repositioning!
+		  		$indel->{ref} = substr($reference, $i, $indel_len);
+		  	}
+		}
+		
+		if($new_pos != $pos) {
+			#$num_indels_repositioned++;
+			my $shift = $pos - $new_pos + 1;
+			#print STDERR "Deletion ($chr:$pos) left-shifted of $shift bp\n";
+		}	
+		
+		$indel->{seq} = ('-' x $indel_len);
+		$indel->{pos} = $new_pos;
+		#$indel->{end} = $indel->{start} + $indel->{len} - 1;
+	}
+	elsif($type eq "ins") {
+	  	my $left_seq  = substr($reference, 0, $p);
+		my $right_seq = substr($reference, $p);
+		my $true_indel_seq = $left_seq . $indel->{seq} . $right_seq;
+		$new_pos = $l+$p;
+		
+		## reposition the candidate variant to a canonical (leftmost) position
+		my $left_hyplotype;
+		my $right_hyplotype;
+		my $new_indel_seq;
+		my $ins;
+		for (my $sft = 1; $sft <= $delta; $sft++) {
+			my $i = $p-$sft;
+			$left_hyplotype  = substr($reference, 0, $i);
+		  	$right_hyplotype = substr($reference, $i);
+		  	$ins = substr($true_indel_seq, $i, $indel_len);
+		  	$new_indel_seq = $left_hyplotype . $ins . $right_hyplotype;
+				
+		  	#print "$true_indel_seq";
+		    #print "$new_indel_seq";
+		  	if ($true_indel_seq eq $new_indel_seq) {
+		  		#print "Repositioning!\n";
+		  		$new_pos = $l+$i; # repositioning!
+		  		$indel->{seq} = $ins;
+		  	}
+		}
+		
+		if($new_pos != $pos) {
+			#$num_indels_repositioned++;
+			my $shift = $pos - $new_pos + 1;
+			#print STDERR "Insertion ($chr:$pos) left-shifted of $shift bp\n";
+		}
+		
+		$indel->{ref} = ('-' x $indel_len);
+		$indel->{pos} = $new_pos;
+		#$indel->{end} = $indel->{start};
+	}
+}
+
+## return unique elements of input list
+#####################################################
+
+sub uniq {
+    return keys %{{ map { $_ => 1 } @_ }};
+}
+
+# binary-search
+##########################################
+sub binarySearch {
+
+	my $x = $_[0];	
+	my $A = $_[1];
+	my $l = $_[2];
+	my $r = $_[3];
+	
+	while($l<=$r) {
+		my $m = floor( ($l+$r)/2 );
+		my $f = @$A[$m];
+		if($f->{end} < $x) { $l = $m+1; }
+		elsif($f->{start} > $x)  { $r = $m-1; }
+		else { 
+			return $m; 
+		}
+	}
+	return -1;
+}
+
+## return true if mutation is in target (start or end
+## positions intersect the target), false otherwise
+#####################################################
+
+sub inTarget {
+	my $mutation = $_[0];
+	my $exons = $_[1];
+	
+	my $chr = $mutation->{chr};
+	my $pos = $mutation->{pos};
+	my $len = $mutation->{len};
+	
+	my $result = "false";
+	
+	my @array = @{$exons->{$chr}};
+	
+	my $flag1 = binarySearch($pos, \@array, 0, (scalar @array)-1);
+	my $flag2 = binarySearch($pos+$len, \@array, 0, (scalar @array)-1);
+	
+	if( ($flag1 != -1) || ($flag2 != -1) ) { $result = "true"; }
+	
+	#foreach my $exon (@{$exons->{$chr}}) {
+		
+	#	my $s = $exon->{start};
+	#	my $e = $exon->{end};
+		
+	#	if( ($s <= $pos) && ($pos <= $e) ) { 
+	#		$result = "true"; 
+	#		last;
+	#	}
+	#}
+	
+	return $result;
+}
+
+# auxilary routine to sort variants (Lexicographically)
+# by chromosome and then by position
+##########################################
+
+sub bychrpos {
+	my ($a, $b, $h) = @_;
+	my $result;
+	if ( ($h->{$a}->{chr} =~ m/^-?\d+\z/) && ($h->{$b}->{chr} =~ m/^-?\d+\z/) ) {
+		$result = ( ($h->{$a}->{chr} <=> $h->{$b}->{chr}) || ($h->{$a}->{pos} <=> $h->{$b}->{pos}) );
+	}
+	else {
+		$result = ( ($h->{$a}->{chr} cmp $h->{$b}->{chr}) || ($h->{$a}->{pos} <=> $h->{$b}->{pos}) );
+	}
+	return $result;
+}
+
+# print total time elapsed 
+##########################################
+
+sub elapsedTime {
+	my $time_taken = $_[0];
+	my $tool = $_[1];
+
+	my $hours = $time_taken / 3600;
+	my $days = $hours / 24; 
+	$hours = $hours % 24;
+	my $seconds = $time_taken % 3600;
+	my $minutes = $seconds / 60;
+	$seconds    = $seconds % 60;
+
+	$days = floor($days);
+	$hours = floor($hours);
+	$minutes = floor($minutes);
+	$seconds = floor($seconds);
+
+	print STDERR "$tool elapsed time: $days day(s) $hours hour(s) $minutes minute(s) $seconds second(s)\n";
+}
+
+
+
+# compute genotype info in VCF format (GT field)
+##########################################
+
+#0/0 - the sample is homozygous reference
+#0/1 - the sample is heterozygous, carrying 1 copy of each of the REF and ALT alleles
+#1/1 - the sample is homozygous alternate
+#1/2 - the sample is heterozygous, carrying 1 copy of the REF and 2 ALT alleles
+
+sub genotype {
+	my $R = $_[0]; # reference coverage
+	my $A = $_[1]; # alternative coverage
+	my $O = $_[2]; # other coverage (>0 for multiple alleles)
+	my $Z = $_[3]; # zygosity (het/hom)
+	my $GT = $_[4]; # GT field on VCF format
+	
+	if($R>0) {
+		if($A>0 && $O==0) { $Z = "het"; $GT = "0/1"; }
+		elsif($A>0 && $O>0) { $Z = "het"; $GT = "0/1"; }
+		elsif($A==0 && $O==0) { $Z = "hom"; $GT = "0/0"; }
+		elsif($A==0 && $O>0) { $Z = "hom"; $GT = "0/0"; }
+	}
+	elsif($R==0) { 
+		if($A>0 && $O==0) { $Z = "het"; $GT = "1/1"; }
+		elsif($A>0 && $O>0) { $Z = "het"; $GT = "1/1"; }
+		elsif($A==0 && $O==0) { $Z = "hom"; $GT = "."; }
+		elsif($A==0 && $O>0) { $Z = "hom"; $GT = "."; }
+	}
+	return ($Z, $GT);
+}
+
+
+# fisher_yates_shuffle( \@array ) : 
+# generate a random permutation of @array in place
+##########################################
+
+sub fisher_yates_shuffle {
+    my $array = shift;
+    my $i;
+    for ($i = @$array; --$i; ) {
+        my $j = int rand ($i+1);
+        next if $i == $j;
+        @$array[$i,$j] = @$array[$j,$i];
+    }
+}
+
+# log 10
+##########################################
+
+sub Log10 {
+        my $n = shift;
+        return log($n)/log(10);
+}
+
+1;
